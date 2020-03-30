fc4ed2eb4bcf569ffb111db8bedb2abe21886cbd
小針隼一郎 <junichiro@kohari.jp>
2020-03-31 08:11:36 +0900

    画像表示ページ準備

---
+++ pages/dogs/_breed/index.vue
@@ -0,0 +1,25 @@
+<template>
+    <section class="container">
+        <div class="columns is-multiline">
+        </div>
+    </section>
+</template>
+ 
+<script>
+import dogApi from '@/api/dog';
+import {mapState} from 'vuex';
+
+export default {
+  //   async fetch ({ store, params, $axios }) {
+  //   const url = 'https://dog.ceo/api/breeds/list/all';
+  //   await $axios.$get(url)
+  //     .then(json => {
+  //       console.log(json)
+  //       store.commit('breed_list_update', json.message)
+  //     })
+  //     .catch(e => ({ error: e }))
+  // },
+  // computed: mapState(['breed_list'])
+}
+
+</script>
